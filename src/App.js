b07843010dcff320bf12a92dfd225ac3939ce661
gopilagah15 <157889090+gopilagah15@users.noreply.github.com>
2024-09-25 12:52:18 +0530

    ttt=o

--- src/App.js
+++ src/App.js
@@ -1,29 +1,4 @@
-// import React from 'react'
-// import Navbar from './component/Navbar'
-// import News from './component/News'
-// import { BrowserRouter, Route, Routes } from 'react-router-dom'
-// // import About from './component/About'
-
-// const App = () => {
-//   return (
-//     <>
-//     <BrowserRouter>
-//     <Navbar/>
-//     <Routes>
-//       <Route exact path='/' element={<News/>}></Route>
-//     </Routes>
-//     </BrowserRouter>
-     
-//       {/* <About/> */}
-//       {/* <News/> */}
-//     </>
-//   )
-// }
-
-// export default App
-
-
-import React, { Component } from 'react'
+ import React, { Component } from 'react'
  import Navbar from './component/Navbar'
  import News from './component/News'
  import { BrowserRouter, Route, Routes } from 'react-router-dom' 
--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -8,6 +8,8 @@ export class NewsItem extends Component {
        <div className="card" >
   <img src={imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
+    {/* <h5 className="card-title">title</h5>
+    <p className="card-text">description</p> */}
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
     <a href="/" className="btn btn-primary">Go somewhere</a>
