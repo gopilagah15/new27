f843e53a732f1f07e823396f539a791b10b03b02
gopilagah15 <157889090+gopilagah15@users.noreply.github.com>
2024-09-25 12:36:55 +0530

    donee

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React from 'react'
-import Navbar from './component/Navbar'
-import News from './component/News'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-// import About from './component/About'
+// import React from 'react'
+// import Navbar from './component/Navbar'
+// import News from './component/News'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// // import About from './component/About'
 
-const App = () => {
-  return (
-    <>
-    <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route exact path='/' element={<News/>}></Route>
-    </Routes>
-    </BrowserRouter>
+// const App = () => {
+//   return (
+//     <>
+//     <BrowserRouter>
+//     <Navbar/>
+//     <Routes>
+//       <Route exact path='/' element={<News/>}></Route>
+//     </Routes>
+//     </BrowserRouter>
      
-      {/* <About/> */}
-      {/* <News/> */}
-    </>
-  )
+//       {/* <About/> */}
+//       {/* <News/> */}
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import React, { Component } from 'react'
+ import Navbar from './component/Navbar'
+ import News from './component/News'
+ import { BrowserRouter, Route, Routes } from 'react-router-dom' 
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+           <BrowserRouter>
+           <Navbar/>
+           <Routes>
+             <Route exact path='/' element={<News/>}></Route>
+           </Routes>
+           </BrowserRouter>
+        
+           </>
+    )
+  }
 }
 
 export default App
