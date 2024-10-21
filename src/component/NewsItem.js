dfe7932bf8e81d613f791f2fdfb7d9a6f7b54ae1
gopilagah15 <157889090+gopilagah15@users.noreply.github.com>
2024-10-21 06:00:30 +0530

    djfkjg

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -2,23 +2,41 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title,description,imageUrl} = this.props;
     return (
-      <>
-       <div className="card" >
-  <img src={imageUrl} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    {/* <h5 className="card-title">title</h5>
-    <p className="card-text">description</p> */}
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href="/" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
- 
-      </>
+      <div>
+        
+      </div>
     )
   }
 }
 
 export default NewsItem
+
+
+
+
+
+
+
+// import React, { Component } from 'react'
+
+// export class NewsItem extends Component {
+//   render() {
+//     let {title,description,imageUrl} = this.props;
+//     return (
+//       <>
+//        <div className="card" >
+//   <img src={imageUrl} className="card-img-top" alt="..."/>
+//   <div className="card-body"> 
+//     <h5 className="card-title">{title}</h5>
+//     <p className="card-text">{description}</p>
+//     <a href="/" className="btn btn-primary">Go somewhere</a>
+//   </div>
+// </div>
+ 
+//       </>
+//     )
+//   }
+// }
+
+// export default NewsItem
